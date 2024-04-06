2744feb5cf9e2c71ec294bafc768206b455c1a41
sammelo2000 <108836524+sammelo2000@users.noreply.github.com>
2024-04-06 22:37:09 +0200

    update

--- blog/app/src/main/java/com/example/blog/MainActivity.java
+++ blog/app/src/main/java/com/example/blog/MainActivity.java
@@ -2,13 +2,46 @@ package com.example.blog;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static int  SPLASH_SCREEN = 5000;
+    Animation bottomAnim, topAnim;
+    ImageView splashScreenImage;
+    TextView logo,motto;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Animations
+        topAnim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+
+        //hooks
+        splashScreenImage = findViewById(R.id.splashScreenImage);
+        logo = findViewById(R.id.logoText);
+        motto = findViewById(R.id.motto);
+
+        splashScreenImage.setAnimation(topAnim);
+        logo.setAnimation(bottomAnim);
+        motto.setAnimation(bottomAnim);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, WelcomePage.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_SCREEN);
+
     }
 }
