295f744795babf7a6ccde2ade6365865a3269e71
Matshuba <md22000628@studentmail.biust.ac.bw>
2024-04-08 01:56:14 +0200

    update

--- blog/app/src/main/java/com/example/blog/ContentPage.java
+++ blog/app/src/main/java/com/example/blog/ContentPage.java
@@ -7,18 +7,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class ContentPage extends AppCompatActivity {
+    ArrayList<ContentModel>contentModels;
+    Content_Adapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_content_page);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        contentModels = new ArrayList<>();
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new Content_Adapter(this, contentModels);
+        recyclerView.setAdapter(adapter);
+
+
+
+
+
     }
 }
