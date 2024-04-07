220e5f9d8d66c8dedce5eedd39f4577d105f5340
sammelo2000 <108836524+sammelo2000@users.noreply.github.com>
2024-04-07 23:58:52 +0200

    done with login and signup ui
    splash screen, login and signup ui

--- blog/app/src/main/java/com/example/blog/LoginPage.java
+++ blog/app/src/main/java/com/example/blog/LoginPage.java
@@ -2,13 +2,50 @@ package com.example.blog;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityOptions;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Pair;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.ImageView;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class LoginPage extends AppCompatActivity {
-
+    Button signUpBtn, loginBtn;
+    TextView logo, motto;
+    ImageView loginimage;
+    TextInputLayout email,password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+
+        signUpBtn = findViewById(R.id.login_signUp);
+        email= findViewById(R.id.loginEmail);
+        password = findViewById(R.id.loginPassword);
+        loginimage = findViewById(R.id.loginImage);
+        logo = findViewById(R.id.loginWelcome);
+        motto = findViewById(R.id.loginDescText);
+        loginBtn = findViewById(R.id.login_btn);
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginPage.this, SignUpPage.class);
+                Pair[] pairs = new Pair[7];
+
+                pairs[0] = new Pair<View,String>(loginimage,"splashScreenImaageAnim");
+                pairs[1] = new Pair<View,String>(email,"loginEmail_trans");
+                pairs[2] = new Pair<View,String>(password,"loginPassword_trans");
+                pairs[3] = new Pair<View,String>(loginBtn,"login_buttonTrans");
+                pairs[4] = new Pair<View,String>(motto,"login_descText");
+                pairs[5] = new Pair<View,String>(signUpBtn,"loginSignUp_trans");
+                pairs[6] = new Pair<View,String>(logo,"logoText_Animation");
+                ActivityOptions opt = ActivityOptions.makeSceneTransitionAnimation(LoginPage.this,pairs);
+                startActivity(intent,opt.toBundle());
+            }
+        });
     }
 }
--- blog/app/src/main/java/com/example/blog/SignUpPage.java
+++ blog/app/src/main/java/com/example/blog/SignUpPage.java
@@ -2,13 +2,52 @@ package com.example.blog;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityOptions;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.util.Pair;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class SignUpPage extends AppCompatActivity {
+    Button haveAcc,signUpBtn;
+    TextView logo, motto;
+    TextInputLayout email,password;
+    ImageView signUpImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up_page);
+
+        haveAcc = findViewById(R.id.alreadHaveAcc);
+        email = findViewById(R.id.signUp_email);
+        password = findViewById(R.id.signUp_password);
+        logo = findViewById(R.id.signUp_logo);
+        motto = findViewById(R.id.signUp_motto);
+        signUpImage = findViewById(R.id.signUp_image);
+        signUpBtn = findViewById(R.id.singUp_button);
+
+        haveAcc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignUpPage.this,LoginPage.class);
+                Pair[] pairs = new Pair[7];
+
+                pairs[0] = new Pair<View,String>(signUpImage,"splashScreenImaageAnim");
+                pairs[1] = new Pair<View,String>(email,"loginEmail_trans");
+                pairs[2] = new Pair<View,String>(password,"loginPassword_trans");
+                pairs[3] = new Pair<View,String>(signUpBtn,"login_buttonTrans");
+                pairs[4] = new Pair<View,String>(motto,"login_descText");
+                pairs[5] = new Pair<View,String>(haveAcc,"loginSignUp_trans");
+                pairs[6] = new Pair<View,String>(logo,"logoText_Animation");
+                ActivityOptions opt = ActivityOptions.makeSceneTransitionAnimation(SignUpPage.this,pairs);
+                startActivity(intent,opt.toBundle());
+            }
+        });
     }
 }
